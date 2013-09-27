d06d2b67a02dafc9b8cd49f9a2d6582f6a55e87c
Aydin Hassan <aydin@hotmail.co.uk>
2013-09-27 23:09:03 +0100

    Add product to cart

---
+++ src/MageMocker/Service/AddToCartService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MageMocker\Service;
+
+use MageMocker\Service\ServiceInterface;
+
+/**
+ * Class AddToCartService
+ * @package MageMocker\Service
+ * @author Aydin Hassan <aydin@hotmail.co.uk>
+ */
+class AddToCartService extends AbstractService implements ServiceInterface {
+
+    /**
+     * Add product to cart
+     */
+    public function mock()
+    {
+        $config     = $this->getConfigObject();
+        $cart       = Mage::getSingleton('checkout/cart');
+        $product    = new Mage_Catalog_Model_Product();
+
+        $product->load($config->getProductId());
+
+        $cart->addProduct($product, array('qty' => $config->getQty()));
+        $cart->save();
+    }
+
+    /**
+     * Save Cart
+     * @param $cart
+     */
+    public function save($cart)
+    {
+        $cart->save();
+    }
+
+} 

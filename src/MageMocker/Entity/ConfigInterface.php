1dac4b08252873624220b894cd37e87c8761fa29
Aydin Hassan <aydin@hotmail.co.uk>
2013-09-27 22:35:49 +0100

    Config Interface for all configs

---
+++ src/MageMocker/Entity/ConfigInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MageMocker\Entity;
+
+/**
+ * Interface ConfigInterface
+ * @package MageMocker\Entity
+ * @author Aydin Hassan <aydin@hotmail.co.uk>
+ */
+interface ConfigInterface {
+
+} 
